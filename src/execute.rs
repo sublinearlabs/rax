da708817153a46a65b90db531bc2c6f1306ae78b
Wisdom Ogwu <iammadab@gmail.com>
2025-12-01 17:08:32 +0100

    using blackhole sink for x0 writes

--- src/execute.rs
+++ src/execute.rs
@@ -4,7 +4,7 @@ impl VM {
     pub(crate) fn execute_instruction(&mut self, insn: Instruction) {
         match insn.opcode {
             Opcode::Add => {
-                self.set_reg(insn.rd, self.reg(insn.rs1) + self.reg(insn.rs2));
+                *self.reg_mut(insn.rd) = self.reg(insn.rs1) + self.reg(insn.rs2);
             }
 
             // TODO remove the earger check once all opcodes have been implemented
@@ -20,8 +20,8 @@ mod test {
     #[test]
     fn test_add_instruction() {
         let mut vm = VM::init();
-        vm.set_reg(3, 12);
-        vm.set_reg(5, 32);
+        *vm.reg_mut(3) = 12;
+        *vm.reg_mut(5) = 32;
         // r8 = r3 + r5
         vm.execute_instruction(Instruction::new(Opcode::Add).rs1(3).rs2(5).rd(8));
         assert_eq!(vm.reg(8), 12 + 32);
