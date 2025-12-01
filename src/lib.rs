34e1a39855b0c7686cb6cc59a91360d33a56477d
Wisdom Ogwu <40731160+iammadab@users.noreply.github.com>
2025-12-01 16:44:13 +0100

    support for imm (#3)
    * add the immediate to the instruction
    
    * move execute into its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod execute;
+
 #[derive(Default)]
 struct VM {
     registers: [u64; 32],
@@ -18,17 +20,6 @@ impl VM {
             self.registers[idx] = val;
         }
     }
-
-    fn execute_instruction(&mut self, insn: Instruction) {
-        match insn.opcode {
-            Opcode::Add => {
-                self.set_reg(insn.rd, self.reg(insn.rs1) + self.reg(insn.rs2));
-            }
-
-            // TODO remove the earger check once all opcodes have been implemented
-            _ => {}
-        }
-    }
 }
 
 // RISCV insturction
@@ -37,16 +28,18 @@ struct Instruction {
     rd: usize,
     rs1: usize,
     rs2: usize,
+    imm: u64,
 }
 
 impl Instruction {
     /// Build a new instruction
-    fn new(opcode: Opcode, rd: usize, rs1: usize, rs2: usize) -> Self {
+    fn new(opcode: Opcode, rd: usize, rs1: usize, rs2: usize, imm: u64) -> Self {
         Self {
             opcode,
             rd,
             rs1,
             rs2,
+            imm,
         }
     }
 }
@@ -67,8 +60,6 @@ enum Opcode {
 
 #[cfg(test)]
 mod tests {
-    use crate::Opcode;
-
     use super::*;
 
     #[test]
@@ -93,15 +84,5 @@ mod tests {
         // write to register 0
         vm.set_reg(0, 20);
         assert_eq!(vm.reg(0), 0);
-    }
-
-    #[test]
-    fn test_add_instruction() {
-        let mut vm = VM::init();
-        vm.set_reg(3, 12);
-        vm.set_reg(5, 32);
-        // r8 = r3 + r5
-        vm.execute_instruction(Instruction::new(Opcode::Add, 8, 3, 5));
-        assert_eq!(vm.reg(8), 12 + 32);
     }
 }
