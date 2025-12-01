9fa835c1cd3081193079ffa348aa748acc7640fd
Wisdom Ogwu <iammadab@gmail.com>
2025-12-01 16:41:54 +0100

    move execute into its own file

---
+++ src/execute.rs
@@ -0,0 +1,29 @@
+use crate::{Instruction, Opcode, VM};
+
+impl VM {
+    pub(crate) fn execute_instruction(&mut self, insn: Instruction) {
+        match insn.opcode {
+            Opcode::Add => {
+                self.set_reg(insn.rd, self.reg(insn.rs1) + self.reg(insn.rs2));
+            }
+
+            // TODO remove the earger check once all opcodes have been implemented
+            _ => {}
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{Instruction, Opcode, VM};
+
+    #[test]
+    fn test_add_instruction() {
+        let mut vm = VM::init();
+        vm.set_reg(3, 12);
+        vm.set_reg(5, 32);
+        // r8 = r3 + r5
+        vm.execute_instruction(Instruction::new(Opcode::Add, 8, 3, 5, 0));
+        assert_eq!(vm.reg(8), 12 + 32);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod execute;
+
 #[derive(Default)]
 struct VM {
     registers: [u64; 32],
@@ -16,17 +18,6 @@ impl VM {
     fn set_reg(&mut self, idx: usize, val: u64) {
         if idx != 0 {
             self.registers[idx] = val;
-        }
-    }
-
-    fn execute_instruction(&mut self, insn: Instruction) {
-        match insn.opcode {
-            Opcode::Add => {
-                self.set_reg(insn.rd, self.reg(insn.rs1) + self.reg(insn.rs2));
-            }
-
-            // TODO remove the earger check once all opcodes have been implemented
-            _ => {}
         }
     }
 }
@@ -69,8 +60,6 @@ enum Opcode {
 
 #[cfg(test)]
 mod tests {
-    use crate::Opcode;
-
     use super::*;
 
     #[test]
@@ -95,15 +84,5 @@ mod tests {
         // write to register 0
         vm.set_reg(0, 20);
         assert_eq!(vm.reg(0), 0);
-    }
-
-    #[test]
-    fn test_add_instruction() {
-        let mut vm = VM::init();
-        vm.set_reg(3, 12);
-        vm.set_reg(5, 32);
-        // r8 = r3 + r5
-        vm.execute_instruction(Instruction::new(Opcode::Add, 8, 3, 5, 0));
-        assert_eq!(vm.reg(8), 12 + 32);
     }
 }
