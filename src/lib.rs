38a8f57557a9ea1017612d2faa99bab1e705f7c1
Wisdom Ogwu <40731160+iammadab@users.noreply.github.com>
2025-12-01 18:45:41 +0100

    feat: memory (#6)
    * mem file
    
    * implement basic memory functions
    
    * test memory
    
    * add memory to the vm
    
    * impl memory operations on the vm
    
    * test memory le load
    
    * rename
    
    * fix comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
+use crate::memory::Memory;
+
 mod execute;
+mod memory;
 
 #[derive(Default)]
 struct VM {
     registers: [u64; 32],
+    memory: Memory,
     x0_sink: u64, // blackhole for writes to x0
 }
 
@@ -12,16 +16,35 @@ impl VM {
         Self::default()
     }
 
+    /// Returns the current value at the idx register
     fn reg(&self, idx: usize) -> u64 {
         if idx == 0 { 0 } else { self.registers[idx] }
     }
 
+    /// Returns a mutable reference to the idx register
     fn reg_mut(&mut self, idx: usize) -> &mut u64 {
         if idx == 0 {
             &mut self.x0_sink
         } else {
             &mut self.registers[idx]
         }
+    }
+
+    /// Reads 64 bytes from memory at the given addr
+    /// assumes value at memory address is the LSB
+    fn mem(&self, addr: usize) -> u64 {
+        let mut result = 0_u64;
+        for i in 0..8 {
+            let byte = self.memory.read((addr + i) as u64);
+            result |= (byte as u64) << (i * 8);
+        }
+        result
+    }
+
+    /// Returns a mutable reference to a single byte at the given
+    /// memory addr
+    fn mem_mut(&mut self, addr: usize) -> &mut u8 {
+        self.memory.mem_mut(addr as u64)
     }
 }
 
@@ -102,5 +125,22 @@ mod tests {
         // write to register 0
         *vm.reg_mut(0) = 20;
         assert_eq!(vm.reg(0), 0);
+    }
+
+    #[test]
+    fn test_memory_loading_le() {
+        let mut vm = VM::init();
+
+        let num = 4_u64;
+        let num_bytes = num.to_le_bytes();
+
+        // write to memory
+        let addr = 0;
+        for i in 0..8 {
+            *vm.mem_mut(addr + i) = num_bytes[i];
+        }
+
+        // read from memory
+        assert_eq!(vm.mem(addr), num);
     }
 }
---
+++ src/memory.rs
@@ -0,0 +1,117 @@
+use std::{char::MAX, collections::HashMap};
+
+/// Number of bits to describe entries in a page
+const PAGE_SHIFT: u64 = 12;
+/// Total number of entries in a page
+const PAGE_SIZE: usize = 1 << PAGE_SHIFT;
+/// Mask to get the last `PAGE_SHIFT` bits of an address
+const PAGE_MASK: u64 = (PAGE_SIZE as u64) - 1;
+/// Max memory address
+const MAX_ADDR: u64 = u64::MAX - 1;
+
+type Page = Box<[u8; PAGE_SIZE]>;
+
+#[derive(Default)]
+pub(crate) struct Memory {
+    pages: HashMap<u64, Page>,
+}
+
+impl Memory {
+    /// Return the page index given the address
+    #[inline]
+    fn page_idx(addr: u64) -> u64 {
+        // addr = [PAGE_ID][PAGE_SHIFT]
+        addr >> PAGE_SHIFT
+    }
+
+    /// Return the entry index within a page
+    /// given an address
+    #[inline]
+    fn page_offset(addr: u64) -> usize {
+        (addr & PAGE_MASK) as usize
+    }
+
+    /// Returns a mutable reference to a page given an address
+    /// lazy allocates the page if needed
+    #[inline]
+    fn ensure_page(&mut self, idx: u64) -> &mut Page {
+        self.pages
+            .entry(idx)
+            .or_insert_with(|| Box::new([0; PAGE_SIZE]))
+    }
+
+    /// Read a single byte. Defaults to 0 if page doesn't exist
+    pub(crate) fn read(&self, addr: u64) -> u8 {
+        if addr > MAX_ADDR {
+            panic!("read out of range: 0x{:x}", addr);
+        }
+        let idx = Self::page_idx(addr);
+        let offset = Self::page_offset(addr);
+        self.pages.get(&idx).map(|p| p[offset]).unwrap_or(0)
+    }
+
+    /// Returns a mutable reference to a single byte at the given
+    /// memory addr
+    pub(crate) fn mem_mut(&mut self, addr: u64) -> &mut u8 {
+        if addr > MAX_ADDR {
+            panic!("write out of range: 0x{:x}", addr);
+        }
+        let idx = Self::page_idx(addr);
+        let offset = Self::page_offset(addr);
+        &mut self.ensure_page(idx)[offset]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_write_and_read_u8() {
+        let mut mem = Memory::default();
+
+        // write
+        *mem.mem_mut(0x1000) = 0xAB;
+        *mem.mem_mut(0x1001) = 0xCD;
+        assert_eq!(mem.pages.len(), 1);
+
+        // read
+        assert_eq!(mem.read(0x1000), 0xAB);
+        assert_eq!(mem.read(0x1001), 0xCD);
+
+        // read unmapped
+        assert_eq!(mem.read(0x7F3A_9C02_B47D_E610), 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_read_out_of_range() {
+        let mem = Memory::default();
+        mem.read(u64::MAX);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_write_out_of_range() {
+        let mut mem = Memory::default();
+        *mem.mem_mut(u64::MAX) = 0;
+    }
+
+    #[test]
+    fn test_cross_page_write() {
+        let mut mem = Memory::default();
+
+        // force boundary cross
+        // (PAGE_SIZE - 4)..(PAGE_SIZE + 4)
+        let start = PAGE_SIZE as u64 - 4;
+        for i in 0..8 {
+            *mem.mem_mut(start + i) = i as u8;
+        }
+        assert_eq!(mem.pages.len(), 2);
+
+        // verify
+        for i in 0..8 {
+            assert_eq!(mem.read(start + i), i as u8);
+        }
+    }
+}
