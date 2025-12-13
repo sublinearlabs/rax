facee8dd1687ac801bab4d6a52733d241f3afecc
developeruche <developeruche@gmail.com>
2025-12-13 11:37:59 +0100

    remove targets

--- rust-bin/exec-block/target/riscv64imac-unknown-none-elf/release/deps/fib-ee78c6b533543d83.d
+++
@@ -1,5 +0,0 @@
-/Users/gregg/Documents/work/sublinear/riscv/rust-bin/fib/target/riscv64imac-unknown-none-elf/release/deps/fib-ee78c6b533543d83.d: src/main.rs
-
-/Users/gregg/Documents/work/sublinear/riscv/rust-bin/fib/target/riscv64imac-unknown-none-elf/release/deps/fib-ee78c6b533543d83: src/main.rs
-
-src/main.rs:
--- rust-bin/fib/target/riscv64imac-unknown-none-elf/release/fib.d
+++
@@ -1,1 +0,0 @@
-/Users/gregg/Documents/work/sublinear/riscv/rust-bin/fib/target/riscv64imac-unknown-none-elf/release/fib: /Users/gregg/Documents/work/sublinear/riscv/rust-bin/fib/src/main.rs
