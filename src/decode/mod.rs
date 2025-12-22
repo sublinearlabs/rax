b997c02a47240f79d86ef276e0184775349631e2
Wisdom Ogwu <40731160+iammadab@users.noreply.github.com>
2025-12-22 22:43:21 +0100

    refactor decode for a extension (rv64a + rv32a) (#41)
    * define the a extension opcodes
    
    * impl atomics decoding

--- src/decode/mod.rs
+++ src/decode/mod.rs
@@ -26,6 +26,9 @@ fn decode(insn: u32) -> Instruction {
         0b0010111 => decode_auipc(insn),
         0b1110011 => decode_system(insn),
         0b0001111 => decode_fence(insn),
+
+        0b0101111 => decode_atomics(insn),
+
         _ => Instruction::Illegal(insn),
     }
 }
@@ -232,4 +235,42 @@ fn decode_system(insn: u32) -> Instruction {
 
 fn decode_fence(_insn: u32) -> Instruction {
     Instruction::Fence
+}
+
+fn decode_atomics(insn: u32) -> Instruction {
+    let funct5 = funct7(insn) >> 2;
+
+    let rd = rd(insn);
+    let rs1 = rs1(insn);
+    let rs2 = rs2(insn);
+
+    let operand = R { rd, rs1, rs2 };
+
+    match (funct3(insn), funct5) {
+        (0x2, 0x02) if rs2 == 0 => Instruction::LrW(operand),
+        (0x2, 0x03) => Instruction::ScW(operand),
+        (0x2, 0x01) => Instruction::AmoSwapW(operand),
+        (0x2, 0x00) => Instruction::AmoAddW(operand),
+        (0x2, 0x04) => Instruction::AmoXorW(operand),
+        (0x2, 0x0c) => Instruction::AmoAndW(operand),
+        (0x2, 0x08) => Instruction::AmoOrW(operand),
+        (0x2, 0x10) => Instruction::AmoMinW(operand),
+        (0x2, 0x14) => Instruction::AmoMaxW(operand),
+        (0x2, 0x18) => Instruction::AmoMinuW(operand),
+        (0x2, 0x1c) => Instruction::AmoMaxuW(operand),
+
+        (0x3, 0x02) if rs2 == 0 => Instruction::LrD(operand),
+        (0x3, 0x03) => Instruction::ScD(operand),
+        (0x3, 0x01) => Instruction::AmoSwapD(operand),
+        (0x3, 0x00) => Instruction::AmoAddD(operand),
+        (0x3, 0x04) => Instruction::AmoXorD(operand),
+        (0x3, 0x0C) => Instruction::AmoAndD(operand),
+        (0x3, 0x08) => Instruction::AmoOrD(operand),
+        (0x3, 0x10) => Instruction::AmoMinD(operand),
+        (0x3, 0x14) => Instruction::AmoMaxD(operand),
+        (0x3, 0x18) => Instruction::AmoMinuD(operand),
+        (0x3, 0x1c) => Instruction::AmoMaxuD(operand),
+
+        _ => Instruction::Illegal(insn),
+    }
 }
