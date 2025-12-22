193e78ee31bc0cf3c915b8580c0b7b3c91c0d12d
Wisdom Ogwu <iammadab@gmail.com>
2025-12-22 15:58:47 +0100

    impl imm for s, b and j

--- src/decode/mod.rs
+++ src/decode/mod.rs
@@ -1,5 +1,5 @@
 mod insn_formats;
-use crate::util::mask32;
+use crate::util::{mask, mask32};
 use insn_formats::{B, I, J, R, S, Sh, U};
 
 enum Instruction {
@@ -97,17 +97,46 @@ fn imm_i(insn: u32) -> i32 {
 
 #[inline]
 fn imm_s(insn: u32) -> i32 {
-    todo!()
+    // insn[31:25] => imm[11:5]
+    let imm11_5 = (insn >> 25) & mask32(7);
+    // insn[11:7] => imm[4:0]
+    let imm4_0 = (insn >> 7) & mask32(5);
+    // place imm parts
+    let imm = (imm11_5 << 5) | imm4_0;
+    // sign extend 12 bits
+    // does this by placing bit at pos 11 as the sign
+    // then performing an arithmetic shift (preserves the sign bit)
+    ((imm as i32) << 20) >> 20
 }
 
 #[inline]
 fn imm_b(insn: u32) -> i32 {
-    todo!()
+    // insn[31] => imm[12]
+    let imm12 = (insn >> 31) & mask32(1);
+    // insn[7] => imm[11]
+    let imm11 = (insn >> 7) & mask32(1);
+    // insn[30:25] => imm[10:5]
+    let imm10_5 = (insn >> 25) & mask32(6);
+    // insn[11:8] => imm[4:1]
+    let imm4_1 = (insn >> 8) & mask32(4);
+
+    let imm = (imm12 << 12) | (imm11 << 11) | (imm10_5 << 5) | (imm4_1 << 1);
+    ((imm as i32) << 19) >> 19
 }
 
 #[inline]
 fn imm_j(insn: u32) -> i32 {
-    todo!()
+    // insn[31] => imm[20]
+    let imm20 = (insn >> 31) & mask32(1);
+    // insn[19:12] => imm[19:12]
+    let imm19_12 = (insn >> 12) & mask32(8);
+    // insn[20] => imm[11]
+    let imm11 = (insn >> 20) & mask32(1);
+    // insn[30:21] => imm[10:1]
+    let imm10_1 = (insn >> 21) & mask32(1);
+
+    let imm = (imm20 << 20) | (imm19_12 << 12) | (imm11 << 11) | (imm10_1 << 1);
+    ((imm as i32) << 11) >> 11
 }
 
 #[inline]
