383c1c24d5afeff6462184ee1e25a08016658d65
Wisdom Ogwu <iammadab@gmail.com>
2026-01-06 09:46:10 +0100

    fixes

--- src/decode/compressed.rs
+++ src/decode/compressed.rs
@@ -16,11 +16,11 @@ fn decode_compressed(insn: u16) -> Instruction {
         (0b00, 0b000) => dec_c_addi4spn(insn),
         (0b00, 0b001) => dec_c_fld(insn),
         (0b00, 0b010) => dec_c_lw(insn),
-        (0b00, 0b011) => dec_c_flw_ld(insn),
-        (0b00, 0b100) => todo!(),
+        (0b00, 0b011) => dec_c_ld(insn),
+        (0b00, 0b100) => Instruction::Illegal(insn as u32),
         (0b00, 0b101) => dec_c_fsd(insn),
         (0b00, 0b110) => dec_c_sw(insn),
-        (0b00, 0b111) => dec_c_fsw_sd(insn),
+        (0b00, 0b111) => dec_c_sd(insn),
 
         // quadrant 1 (01)
         _ => Instruction::Illegal(insn as u32),
@@ -76,9 +76,7 @@ fn dec_c_lw(insn: u16) -> Instruction {
     Instruction::Lw(I { rd, rs1, imm })
 }
 
-// TODO currently assumes only RV64
-// TODO add support for RV32 decompression
-fn dec_c_flw_ld(insn: u16) -> Instruction {
+fn dec_c_ld(insn: u16) -> Instruction {
     // rd' insn[4:2]
     // rd = rd' + 8
     let rd = (((insn >> 2) & mask16(3)) + 8) as u8;
@@ -120,8 +118,7 @@ fn dec_c_sw(insn: u16) -> Instruction {
     Instruction::Sw(S { rs1, rs2, imm })
 }
 
-// TODO support RV32C
-fn dec_c_fsw_sd(insn: u16) -> Instruction {
+fn dec_c_sd(insn: u16) -> Instruction {
     // rs1' insn[9:7]
     // rs1 = rs1' + 8
     let rs1 = (((insn >> 7) & mask16(3)) + 8) as u8;
