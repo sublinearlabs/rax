b031aca9d03ceb796ea7cd35de57fae15a4c3f4c
Wisdom Ogwu <iammadab@gmail.com>
2026-02-19 18:33:19 +0100

    make insn implement copy

--- src/decode/insn_formats.rs
+++ src/decode/insn_formats.rs
@@ -3,7 +3,7 @@ use serde::Deserialize;
 /// R-type register-register format
 ///
 /// Semantics `rd <- f(rs1, rs2)`
-#[derive(Debug, Clone, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq)]
 pub(crate) struct R {
     pub(crate) rd: u8,
     pub(crate) rs1: u8,
@@ -13,7 +13,7 @@ pub(crate) struct R {
 /// I-type register-immediate format
 ///
 /// `imm` is a sign-extended immediate
-#[derive(Debug, Clone, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq)]
 pub(crate) struct I {
     pub(crate) rd: u8,
     pub(crate) rs1: u8,
@@ -25,7 +25,7 @@ pub(crate) struct I {
 /// `shamt` is the shift amount:
 /// - RV32: 5 bits
 /// - RV64: 6 bits
-#[derive(Debug, Clone, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq)]
 pub(crate) struct Sh {
     pub(crate) rd: u8,
     pub(crate) rs1: u8,
@@ -37,7 +37,7 @@ pub(crate) struct Sh {
 /// `imm` is a sign-extended byte offset
 ///
 /// Semantics `mem[rs1 + imm] <- rs2`
-#[derive(Debug, Clone, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq)]
 pub(crate) struct S {
     pub(crate) rs1: u8,
     pub(crate) rs2: u8,
@@ -51,7 +51,7 @@ pub(crate) struct S {
 /// - Else: fall through to next instruction
 ///
 /// `imm` is the sign-extended PC-relative byte offset
-#[derive(Debug, Clone, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq)]
 pub(crate) struct B {
     pub(crate) rs1: u8,
     pub(crate) rs2: u8,
@@ -65,7 +65,7 @@ pub(crate) struct B {
 /// Semantics:
 /// - `rd <- next_pc`
 /// - `pc <- pc + imm`
-#[derive(Debug, Clone, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq)]
 pub(crate) struct J {
     pub(crate) rd: u8,
     pub(crate) imm: i32,
@@ -76,7 +76,7 @@ pub(crate) struct J {
 /// `imm` represents the sign-extended upper-immediate
 ///
 /// stores the imm already shifted left by 12 bits
-#[derive(Debug, Clone, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq)]
 pub(crate) struct U {
     pub(crate) rd: u8,
     pub(crate) imm: i32,
@@ -85,7 +85,7 @@ pub(crate) struct U {
 /// RF format (floating-point)
 ///
 /// Same register fields as R, but includes rounding mode `rm`
-#[derive(Debug, Clone, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq)]
 pub(crate) struct RF {
     pub(crate) rd: u8,
     pub(crate) rs1: u8,
@@ -94,7 +94,7 @@ pub(crate) struct RF {
 }
 
 /// R4-type (fused multiply-add format)
-#[derive(Debug, Clone, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq)]
 pub(crate) struct R4 {
     pub(crate) rd: u8,
     pub(crate) rs1: u8,
