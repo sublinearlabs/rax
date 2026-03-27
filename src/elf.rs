3d240639f381d62542efd773c2a546f0ade885d9
Ginika-Chinonso <ginika.chinonso.francis@gmail.com>
2026-03-27 16:14:37 +0100

    parse elf file

--- src/elf.rs
+++ src/elf.rs
@@ -52,3 +52,77 @@ pub(crate) fn decode_elf(bytes: &[u8]) -> (MemoryDefault, u64) {
 
     (memory, entry)
 }
+
+struct Segment {
+    data: Vec<u8>,
+    entry: u64,
+    offset: usize,
+    file_size: usize,
+    mem_size: usize,
+}
+
+impl Segment {
+    fn new(data: Vec<u8>, entry: u64, offset: usize, file_size: usize, mem_size: usize) -> Self {
+        Self {
+            data,
+            entry,
+            offset,
+            file_size,
+            mem_size,
+        }
+    }
+}
+
+struct Elf {
+    segments: Vec<Segment>,
+    global_entry: u64,
+}
+
+impl Elf {
+    fn new(segments: Vec<Segment>, global_entry: u64) -> Self {
+        Self {
+            segments,
+            global_entry,
+        }
+    }
+}
+
+/// Parses the elf file
+pub(crate) fn parse_elf(bytes: &[u8]) -> Elf {
+    let file =
+        ElfBytes::<LittleEndian>::minimal_parse(bytes).expect("failed to parse the elf file");
+    let ehdr = file.ehdr;
+
+    assert_eq!(ehdr.class, Class::ELF64);
+    assert_eq!(ehdr.e_type, ET_EXEC);
+    assert_eq!(ehdr.e_machine, EM_RISCV);
+
+    let global_entry = ehdr.e_entry;
+    let mut parsed_segments: Vec<Segment> = vec![];
+
+    // Iterate over all program headers
+    // load header of type `PT_LOAD` to memory
+    let segments = file.segments().expect("has no program headers");
+    for ph in segments.iter() {
+        if ph.p_type != PT_LOAD {
+            continue;
+        }
+
+        let offset = ph.p_offset as usize;
+        let filesz = ph.p_filesz as usize;
+        let vaddr = ph.p_vaddr;
+        let memsz = ph.p_memsz as usize;
+
+        if memsz < filesz {
+            panic!("malformed elf file");
+        }
+
+        if filesz > 0 {
+            let data = &bytes[offset..offset + filesz];
+            let value = Segment::new(data.to_vec(), vaddr, offset, filesz, memsz);
+            parsed_segments.push(value);
+        }
+    }
+
+    Elf::new(parsed_segments, global_entry)
+}
