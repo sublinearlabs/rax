67e714d3275d9a01dca50a8d5a53bea249929d3d
Wisdom Ogwu <iammadab@gmail.com>
2026-04-15 18:30:20 +0100

    define translate_insn

--- src/aot/compiler.rs
+++ src/aot/compiler.rs
@@ -1,7 +1,11 @@
 use crate::decode::Instruction;
+use dynasmrt::x86::Assembler;
 
-struct Compiler {
-    decoded_instructions: Vec<Instruction>,
+fn translate_insns(insns: &[Instruction], assembler: Assembler) {
+    for insn in insns {
+        match insn {
+            Instruction::Add(r) => todo!(),
+            _ => todo!(),
+        }
+    }
 }
-
-impl Compiler {}
