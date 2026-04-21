8daf6d9aea2f2b8d8ef0e9c9a43efd517c80104c
Wisdom Ogwu <iammadab@gmail.com>
2026-04-21 16:54:23 +0100

    add constraints documentation

--- src/aot/mod.rs
+++ src/aot/mod.rs
@@ -5,11 +5,29 @@ use crate::elf::parse_elf;
 pub(crate) mod compiler;
 pub(crate) mod register_mapping;
 
+/// Generate an equivalent x86 ELF file given the path to a riscv elf file
+///
+/// RISCV ELF Constraints
+/// - uncompressed format (without the c extension)
+/// - single code segment
 fn compile_elf(path: &'static str) {
     let bytes = fs::read(path).unwrap();
     let mut elf = parse_elf(&bytes);
+
+    // ELF constrinat
+    // should only work on uncompressed
+    // there should be only one executable region
+
+    let mut no_executable = 0;
+
     for segment in &mut elf.segments {
         if segment.is_executable {
+            // TODO: figure out how to get rid of this restriction
+            no_executable += 1;
+            if no_executable > 1 {
+                panic!("compiler only works with elfs that have a single code segment");
+            }
+
             segment.decode();
             dbg!(&segment.insns);
         }
