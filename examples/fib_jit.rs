dea560b387d18f72842390ce4ef5ec9a9d4e39bf
Wisdom Ogwu <iammadab@gmail.com>
2026-06-30 17:37:28 +0100

    clean up examples and tests

---
+++ examples/fib_jit.rs
@@ -0,0 +1,11 @@
+use riscv::init_from_elf;
+use riscv_jit::Runner;
+
+const FIB_BINARY: &str = "test-bin/rust-bin/fib/fib-gc";
+
+fn main() {
+    let mut vm = init_from_elf(FIB_BINARY.to_string());
+    let mut runner = Runner::new();
+    runner.run(&mut vm);
+    assert_eq!(vm.exit_code, 0);
+}
